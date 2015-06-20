e6a4537a46284d38a9f36b48b76aebf069ff1374
Jonhnes Lopes <jonhnes@gmail.com>
2015-06-20 12:23:39 -0300

    Show food was finished

--- app/src/main/java/br/ufc/virtual/smd/oqcomer/fragments/MainFragment.java
+++ app/src/main/java/br/ufc/virtual/smd/oqcomer/fragments/MainFragment.java
@@ -3,219 +3,49 @@ package br.ufc.virtual.smd.oqcomer.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import br.ufc.virtual.smd.oqcomer.R;
 import br.ufc.virtual.smd.oqcomer.adapter.ProductListAdapter;
+import br.ufc.virtual.smd.oqcomer.model.DataBase;
 
 public class MainFragment extends Fragment {
-
-    private ListView produtoLista;
-    private int[] banners = {
-            R.drawable.banner_1,
-            R.drawable.banner_2,
-            R.drawable.banner_3,
-            R.drawable.bolo_chocolate,
-            R.drawable.brigadeiro,
-            R.drawable.brownie,
-            R.drawable.cachorro_quente,
-            R.drawable.cajuina,
-            R.drawable.camarao,
-            R.drawable.coxinha,
-            R.drawable.empada,
-            R.drawable.escondidinho_carnedosol,
-            R.drawable.hamburguer,
-            R.drawable.kalzonekalzone,
-            R.drawable.misto_quente,
-            R.drawable.ohmydog,
-            R.drawable.palha_italiana,
-            R.drawable.pao_de_queijo,
-            R.drawable.pastel_de_queijo,
-            R.drawable.picanha,
-            R.drawable.pizzahut_peperoni,
-            R.drawable.po_de_guarana,
-            R.drawable.rock_burguer,
-            R.drawable.salada_fruta,
-            R.drawable.self_service_fisica,
-            R.drawable.sushi_oxente,
-            R.drawable.torta_frango,
-            R.drawable.vitamina_acai
-
-    };
-    private String[] foodNames = {
-            "Pastel de requeijão com carne seca",
-            "Nachos mechicanos a moda da casa",
-            "Escondidinho de camarão",
-            "Bolo de chocolate",
-            "Brigadeiro",
-            "Brownie de chocolate",
-            "Cachorro quente",
-            "Cajuina de verdade",
-            "Camarão alho e óleo",
-            "Coxinha",
-            "Empada",
-            "Escondidinho de carne do sol",
-            "Hamburguer",
-            "Kalzone de calabresa",
-            "Misto quente",
-            "Oh my dog peperoni",
-            "Palha italiana",
-            "Pão de queijo",
-            "Pastel de queijo",
-            "Picanha com batatas",
-            "Pizza de peperoni",
-            "Pó de guaraná",
-            "Rock burger",
-            "Salada de frutas",
-            "Self service",
-            "Sushis variados",
-            "Torta de frango",
-            "Vitamina de Açai"
-    };
-    private String[] restaurantLocal = {
-            "Dom Pastel",
-            "Floresta Gourmet",
-            "Florence l'escale",
-            "Cantina da Química",
-            "Cantina da Tia Jô",
-            "Cantina da SEARA de Ciências",
-            "Cantina da Tia Jô",
-            "Cantina da Química",
-            "Cantina da Tia Jô",
-            "Cantina da Geologia",
-            "Cantina da Eng. de Pesca",
-            "Cantina da Física",
-            "Cantina da Geologia",
-            "Kalzone",
-            "Cantina da SEARA de Ciências",
-            "Oh my dog",
-            "Cantina da Tia Jô",
-            "Cantina da Química",
-            "Cantina da Tia Jô",
-            "Restaurante Alex",
-            "Pizza Hut",
-            "Ponto da Vitamina",
-            "Rock And Burgers",
-            "Cantina da Tia Jô",
-            "Cantina da Física",
-            "Oxente oriente",
-            "Cantina da Química",
-            "Cantina da Eng. de Pesca"
-    };
-    private String[] localDistances = {
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km",
-            "1,1 Km",
-            "0,8 Km",
-            "0,6 Km"
-    };
-    private String[] countComments = {
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15",
-            "41",
-            "18",
-            "15"
-    };
-    private int[] satisfactionFlags = {
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-            R.drawable.the_best_flag,
-            R.drawable.great_flag,
-            R.drawable.very_good_flag,
-    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-        ProductListAdapter adapter = new ProductListAdapter(getActivity(), banners, foodNames, restaurantLocal, localDistances, countComments, satisfactionFlags);
-        produtoLista = (ListView) view.findViewById(R.id.produtoLista);
-        produtoLista.setAdapter(adapter);
+        ProductListAdapter adapter = new ProductListAdapter(
+                getActivity(),
+                DataBase.banners,
+                DataBase.foodNames,
+                DataBase.restaurantLocal,
+                DataBase.localDistances,
+                DataBase.countComments,
+                DataBase.satisfactionFlags
+        );
+        ListView productList = (ListView) view.findViewById(R.id.product_list);
+        productList.setAdapter(adapter);
 
-        produtoLista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = foodNames[+position];
-                Toast.makeText(getActivity(), selectedItem, Toast.LENGTH_SHORT).show();
+                ShowFoodFragment showFoodFragment = ShowFoodFragment.newInstance(+position);
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_container, showFoodFragment).addToBackStack(showFoodFragment.getClass().getName());
+                fragmentTransaction.commit();
             }
         });
 
--- app/src/main/java/br/ufc/virtual/smd/oqcomer/fragments/ShowFoodFragment.java
+++ app/src/main/java/br/ufc/virtual/smd/oqcomer/fragments/ShowFoodFragment.java
@@ -1,35 +1,24 @@
 package br.ufc.virtual.smd.oqcomer.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import br.ufc.virtual.smd.oqcomer.R;
+import br.ufc.virtual.smd.oqcomer.model.DataBase;
 
 public class ShowFoodFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String POSITION_PARAM = "position";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int position;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ShowFoodFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ShowFoodFragment newInstance(String param1, String param2) {
+    public static ShowFoodFragment newInstance(int position) {
         ShowFoodFragment fragment = new ShowFoodFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(POSITION_PARAM, position);
         fragment.setArguments(args);
         return fragment;
     }
@@ -38,16 +27,26 @@ public class ShowFoodFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            position = getArguments().getInt(POSITION_PARAM);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_show_food, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        view.findViewById(R.id.iv_food_banner).setBackgroundResource(DataBase.banners[position]);
+        view.findViewById(R.id.iv_satisfaction_flag).setBackgroundResource(DataBase.satisfactionFlags[position]);
+        ((TextView) view.findViewById(R.id.tv_food_name)).setText(DataBase.foodNames[position]);
+        ((TextView) view.findViewById(R.id.tv_food_description)).setText(DataBase.foodDescriptions[position]);
+        ((TextView) view.findViewById(R.id.tv_restaurant_name)).setText(DataBase.restaurantLocal[position]);
+        ((TextView) view.findViewById(R.id.tv_restaurant_description)).setText(DataBase.restaurantDescriptions[position]);
+        ((TextView) view.findViewById(R.id.tv_food_opinion)).setText(DataBase.foodOpinions[position]);
+        view.findViewById(R.id.iv_satisfaction_type_image).setBackgroundResource(DataBase.foodSatisfactionImage[position]);
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
